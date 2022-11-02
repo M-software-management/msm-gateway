43dbc077eeda74c531f91d61dc87db86e531f48b
lohansimpson <lmichaelson07@gmail.com>
2022-11-01 21:42:03 -0500

    changed stuff

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 import express from "express"
-import {Getusers, Getuser, updateuser} from "../controllers/user.js"
+import {Getusers, Getuser, updateuser, getself} from "../controllers/user.js"
 const router = express.Router()
 
 router.get("/", Getusers)
 router.get("/:id", Getuser)
 router.put("/:id", updateuser)
-
+router.get("/m/1", getself)
 
 
 
