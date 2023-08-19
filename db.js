ac91c34a7b0dec888649e699db5f5d30d613b3f3
lohansimpson <lmichaelson07@gmail.com>
2023-08-19 13:30:50 -0500

    added planetscale-encype-2

--- db.js
+++ db.js
@@ -18,6 +18,7 @@ export const db = mysql.createConnection({
     database: process.env.mysql_database,
     port: process.env.mysql_port,
     insecureAuth : true,
+    ssl_mode: verify_identity,
     ssl  : {
       ca : fs.readFileSync('./ca-certificate.crt'),   
     }
